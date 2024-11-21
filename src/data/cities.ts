c421a3da3518b1be0f014f609d6e50b12059ec93
jullherr <jakob.ullherr@freenet.de>
2024-11-21 10:19:14 +0100

    Exercise 8: Add weather API interfaces and favorite city coordinates

---
+++ src/data/cities.ts
@@ -0,0 +1,8 @@
+export const cityCoordinates = {
+    "Munich": {
+        latitude: 48.137154,
+        longitude: 11.576124
+    }
+}
+
+export type CityName = keyof typeof cityCoordinates;
---
+++ src/types/WeatherData.ts
@@ -0,0 +1,29 @@
+export interface WeatherApiResponse {
+    latitude: number;
+    longitude: number;
+    generationtime_ms: number;
+    utc_offset_seconds: number;
+    timezone: string;
+    elevation: number;
+    current: CurrentWeather;
+    hourly_units: HourlyUnits;
+    hourly: HourlyData;
+}
+
+
+export interface CurrentWeather {
+    time: string;
+    interval: number;
+    relative_humidity_2m: number;
+    wind_speed_10m: number;
+}
+
+export interface HourlyUnits {
+    time: string;
+    temperature_2m: string;
+}
+
+export interface HourlyData {
+    time: readonly string[];
+    temperature_2m: readonly string[];
+}
